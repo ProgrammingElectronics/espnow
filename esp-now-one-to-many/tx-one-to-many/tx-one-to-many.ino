910d971801582df79d91aa45e624ee59b9ab4522
Michael Cheich <mcheich@gmail.com>
2022-09-19 11:36:15 -0400

    effect menu added

--- esp-now-one-to-many/tx-one-to-many/tx-one-to-many.ino
+++ esp-now-one-to-many/tx-one-to-many/tx-one-to-many.ino
@@ -24,7 +24,7 @@
 #define NUMRECEIVERS 20
 
 esp_now_peer_info_t receivers[NUMRECEIVERS] = {};
-char peerSSIDs[20][50]; // Store the SSID of each connected network
+char peerSSIDs[20][50];  // Store the SSID of each connected network
 int RXCnt = 0;
 
 #define CHANNEL 1
@@ -47,16 +47,17 @@ int RXCnt = 0;
 // List Peers menu options
 #define NUM_PEERS_TO_DISPLAY 3
 #define BACK_BUTTON_SPACER 1
+#define NUM_MENU_ITEMS_TO_DISPLAY 3
 
 const byte INCREMENT_BUTTON = 5;
 const byte SELECT_BUTTON = 6;
 const int DEBOUNCE = 250;
 const byte MAX_SELECTIONS = 2;
-const byte LINE_SPACING = 5; // space between each line
+const byte LINE_SPACING = 5;  // space between each line
 const byte MAIN_MENU_LENGTH = 3;
-const byte SELECT_EFFECT_LENGTH = 4;
-const char *MAIN_MENU_OPTIONS[MAIN_MENU_LENGTH] = {"1. List Peers", "2. ReScan", "3. Broadcast"};
-const char *SEL_EFFECT_OPTIONS[SELECT_EFFECT_LENGTH] = {"1. Change Color", "2. Cyclon", "3. Pacifica", "4. Random Reds"};
+const byte SELECT_EFFECT_LENGTH = 5;
+const char *MAIN_MENU_OPTIONS[MAIN_MENU_LENGTH] = { "1. List Peers", "2. ReScan", "3. Broadcast" };
+const char *SEL_EFFECT_OPTIONS[SELECT_EFFECT_LENGTH] = { "1. Change Color", "2. Cyclon", "3. Pacifica", "4. Random Reds", "5. Back" };
 
 /**
  * List Peers
@@ -105,18 +106,14 @@ volatile unsigned long last_incr_button_time = 0;
 volatile unsigned long last_sel_button_time = 0;
 
 // Display
-U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* clock=*/SCL, /* data=*/SDA, /* reset=*/U8X8_PIN_NONE); // High speed I2C
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* clock=*/SCL, /* data=*/SDA, /* reset=*/U8X8_PIN_NONE);  // High speed I2C
 
 // Init ESP Now with fallback
-void InitESPNow()
-{
+void InitESPNow() {
   WiFi.disconnect();
-  if (esp_now_init() == ESP_OK)
-  {
+  if (esp_now_init() == ESP_OK) {
     Serial.println("ESPNow Init Success");
-  }
-  else
-  {
+  } else {
     Serial.println("ESPNow Init Failed");
     // Retry InitESPNow, add a counte and then restart?
     // InitESPNow();
@@ -126,31 +123,25 @@ void InitESPNow()
 }
 
 // Scan for receivers in AP mode
-void ScanForReceivers()
-{
+void ScanForReceivers() {
   int8_t scanResults = WiFi.scanNetworks();
   // reset receivers
   memset(receivers, 0, sizeof(receivers));
   RXCnt = 0;
   Serial.println("");
-  if (scanResults == 0)
-  {
+  if (scanResults == 0) {
     Serial.println("No WiFi devices in AP Mode found");
-  }
-  else
-  {
+  } else {
     Serial.print("Found ");
     Serial.print(scanResults);
     Serial.println(" devices ");
-    for (int i = 0; i < scanResults; ++i)
-    {
+    for (int i = 0; i < scanResults; ++i) {
       // Print SSID and RSSI for each device found
       String SSID = WiFi.SSID(i);
       int32_t RSSI = WiFi.RSSI(i);
       String BSSIDstr = WiFi.BSSIDstr(i);
 
-      if (PRINTSCANRESULTS)
-      {
+      if (PRINTSCANRESULTS) {
         Serial.print(i + 1);
         Serial.print(": ");
         Serial.print(SSID);
@@ -164,8 +155,7 @@ void ScanForReceivers()
       }
       delay(10);
       // Check if the current device starts with `RX`
-      if (SSID.indexOf("RX") == 0)
-      {
+      if (SSID.indexOf("RX") == 0) {
         // SSID of interest
         Serial.print(i + 1);
         Serial.print(": ");
@@ -180,15 +170,13 @@ void ScanForReceivers()
         // Get BSSID => Mac Address of the RX
         int mac[6];
 
-        if (6 == sscanf(BSSIDstr.c_str(), "%x:%x:%x:%x:%x:%x", &mac[0], &mac[1], &mac[2], &mac[3], &mac[4], &mac[5]))
-        {
-          for (int ii = 0; ii < 6; ++ii)
-          {
+        if (6 == sscanf(BSSIDstr.c_str(), "%x:%x:%x:%x:%x:%x", &mac[0], &mac[1], &mac[2], &mac[3], &mac[4], &mac[5])) {
+          for (int ii = 0; ii < 6; ++ii) {
             receivers[RXCnt].peer_addr[ii] = (uint8_t)mac[ii];
           }
         }
-        receivers[RXCnt].channel = CHANNEL; // pick a channel
-        receivers[RXCnt].encrypt = 0;       // no encryption
+        receivers[RXCnt].channel = CHANNEL;  // pick a channel
+        receivers[RXCnt].encrypt = 0;        // no encryption
 
         SSID.toCharArray(peerSSIDs[RXCnt], SSID.length());
 
@@ -197,13 +185,10 @@ void ScanForReceivers()
     }
   }
 
-  if (RXCnt > 0)
-  {
+  if (RXCnt > 0) {
     Serial.print(RXCnt);
     Serial.println(" Receivers(s) found, processing..");
-  }
-  else
-  {
+  } else {
     Serial.println("No Receiver Found, trying again.");
   }
 
@@ -213,15 +198,11 @@ void ScanForReceivers()
 
 // Check if the receiver is already paired with the transmitter.
 // If not, pair the receiver with transmitter
-void manageReceiver()
-{
-  if (RXCnt > 0)
-  {
-    for (int i = 0; i < RXCnt; i++)
-    {
+void manageReceiver() {
+  if (RXCnt > 0) {
+    for (int i = 0; i < RXCnt; i++) {
       Serial.print("Processing: ");
-      for (int ii = 0; ii < 6; ++ii)
-      {
+      for (int ii = 0; ii < 6; ++ii) {
         Serial.print((uint8_t)receivers[i].peer_addr[ii], HEX);
         if (ii != 5)
           Serial.print(":");
@@ -229,51 +210,33 @@ void manageReceiver()
       Serial.print(" Status: ");
       // check if the peer exists
       bool exists = esp_now_is_peer_exist(receivers[i].peer_addr);
-      if (exists)
-      {
+      if (exists) {
         // Receiver already paired.
         Serial.println("Already Paired");
-      }
-      else
-      {
+      } else {
         // Receiver not paired, attempt pair
         esp_err_t addStatus = esp_now_add_peer(&receivers[i]);
-        if (addStatus == ESP_OK)
-        {
+        if (addStatus == ESP_OK) {
           // Pair success
           Serial.println("Pair success");
-        }
-        else if (addStatus == ESP_ERR_ESPNOW_NOT_INIT)
-        {
+        } else if (addStatus == ESP_ERR_ESPNOW_NOT_INIT) {
           // How did we get so far!!
           Serial.println("ESPNOW Not Init");
-        }
-        else if (addStatus == ESP_ERR_ESPNOW_ARG)
-        {
+        } else if (addStatus == ESP_ERR_ESPNOW_ARG) {
           Serial.println("Add Peer - Invalid Argument");
-        }
-        else if (addStatus == ESP_ERR_ESPNOW_FULL)
-        {
+        } else if (addStatus == ESP_ERR_ESPNOW_FULL) {
           Serial.println("Peer list full");
-        }
-        else if (addStatus == ESP_ERR_ESPNOW_NO_MEM)
-        {
+        } else if (addStatus == ESP_ERR_ESPNOW_NO_MEM) {
           Serial.println("Out of memory");
-        }
-        else if (addStatus == ESP_ERR_ESPNOW_EXIST)
-        {
+        } else if (addStatus == ESP_ERR_ESPNOW_EXIST) {
           Serial.println("Peer Exists");
-        }
-        else
-        {
+        } else {
           Serial.println("Not sure what happened");
         }
         delay(100);
       }
     }
-  }
-  else
-  {
+  } else {
     // No receiver found to process
     Serial.println("No receiver found to process");
   }
@@ -281,46 +244,30 @@ void manageReceiver()
 
 uint8_t data = 0;
 // send data
-void sendData()
-{
+void sendData() {
   data++;
-  for (int i = 0; i < RXCnt; i++)
-  {
+  for (int i = 0; i < RXCnt; i++) {
     const uint8_t *peer_addr = receivers[i].peer_addr;
-    if (i == 0)
-    { // print only for first receiver
+    if (i == 0) {  // print only for first receiver
       Serial.print("Sending: ");
       Serial.println(data);
     }
     esp_err_t result = esp_now_send(peer_addr, &data, sizeof(data));
     Serial.print("Send Status: ");
-    if (result == ESP_OK)
-    {
+    if (result == ESP_OK) {
       Serial.println("Success");
-    }
-    else if (result == ESP_ERR_ESPNOW_NOT_INIT)
-    {
+    } else if (result == ESP_ERR_ESPNOW_NOT_INIT) {
       // How did we get so far!!
       Serial.println("ESPNOW not Init.");
-    }
-    else if (result == ESP_ERR_ESPNOW_ARG)
-    {
+    } else if (result == ESP_ERR_ESPNOW_ARG) {
       Serial.println("Invalid Argument");
-    }
-    else if (result == ESP_ERR_ESPNOW_INTERNAL)
-    {
+    } else if (result == ESP_ERR_ESPNOW_INTERNAL) {
       Serial.println("Internal Error");
-    }
-    else if (result == ESP_ERR_ESPNOW_NO_MEM)
-    {
+    } else if (result == ESP_ERR_ESPNOW_NO_MEM) {
       Serial.println("ESP_ERR_ESPNOW_NO_MEM");
-    }
-    else if (result == ESP_ERR_ESPNOW_NOT_FOUND)
-    {
+    } else if (result == ESP_ERR_ESPNOW_NOT_FOUND) {
       Serial.println("Peer not found.");
-    }
-    else
-    {
+    } else {
       Serial.println("Not sure what happened");
     }
     delay(100);
@@ -328,8 +275,7 @@ void sendData()
 }
 
 // callback when data is sent from Transmitter to Receiver
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
-{
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   char macStr[18];
   snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
            mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
@@ -339,6 +285,7 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
+/*
 void displayMenu(const char *menuArray[], byte len)
 {
   u8g2.clearBuffer(); // clear the internal memory
@@ -352,9 +299,26 @@ void displayMenu(const char *menuArray[], byte len)
 
   u8g2.sendBuffer(); // transfer internal memory to the display
 }
+*/
 
-void displayPeers()
-{
+void displayMenu(const char *menuArray[], byte len) {
+  u8g2.clearBuffer();  // clear the internal memory
+
+  int spacing = LINE_SPACING + u8g2.getAscent() + abs(u8g2.getDescent());
+
+  // Only show 3 menu items at once
+  int start = currentSelection / NUM_MENU_ITEMS_TO_DISPLAY * NUM_MENU_ITEMS_TO_DISPLAY;
+  int end = start + NUM_MENU_ITEMS_TO_DISPLAY > len ? len : start + NUM_MENU_ITEMS_TO_DISPLAY;
+
+  for (int i = start; i < end; i++) {
+    u8g2.drawButtonUTF8(0, spacing, currentSelection == i ? U8G2_BTN_INV : U8G2_BTN_BW0, 0, 2, 2, menuArray[i]);
+    spacing += LINE_SPACING + u8g2.getAscent() + abs(u8g2.getDescent());
+  }
+
+  u8g2.sendBuffer();  // transfer internal memory to the display
+}
+
+void displayPeers() {
   u8g2.clearBuffer();
   int spacing = LINE_SPACING + u8g2.getAscent() + abs(u8g2.getDescent());
 
@@ -362,20 +326,11 @@ void displayPeers()
   int start = currentSelection / NUM_PEERS_TO_DISPLAY * NUM_PEERS_TO_DISPLAY;
   int end = start + NUM_PEERS_TO_DISPLAY > RXCnt + BACK_BUTTON_SPACER ? RXCnt + BACK_BUTTON_SPACER : start + NUM_PEERS_TO_DISPLAY;
 
-  Serial.print("End ->");
-  Serial.println(end);
-  Serial.print("RXcnt ->");
-  Serial.println(RXCnt);
-
-  for (int i = start; i < end; i++)
-  {
-    if (i == RXCnt)
-    {
+  for (int i = start; i < end; i++) {
+    if (i == RXCnt) {
       u8g2.drawButtonUTF8(1, spacing, currentSelection == i ? U8G2_BTN_INV : U8G2_BTN_BW0, 0, 2, 2, "Back");
       Serial.println("end printed");
-    }
-    else
-    {
+    } else {
       u8g2.drawButtonUTF8(1, spacing, currentSelection == i ? U8G2_BTN_INV : U8G2_BTN_BW0, 0, 2, 2, peerSSIDs[i]);
       Serial.println("RX printed ");
       Serial.println(i);
@@ -383,14 +338,12 @@ void displayPeers()
     spacing += LINE_SPACING + u8g2.getAscent() + abs(u8g2.getDescent());
   }
 
-  u8g2.sendBuffer(); // transfer internal memory to the display
+  u8g2.sendBuffer();  // transfer internal memory to the display
 }
 
-void IRAM_ATTR incrementButton()
-{
+void IRAM_ATTR incrementButton() {
   incr_button_time = millis();
-  if (incr_button_time - last_incr_button_time > DEBOUNCE)
-  {
+  if (incr_button_time - last_incr_button_time > DEBOUNCE) {
     currentSelection++;
 
     Serial.println(currentSelection);
@@ -398,19 +351,16 @@ void IRAM_ATTR incrementButton()
   }
 }
 
-void IRAM_ATTR selectButton()
-{
+void IRAM_ATTR selectButton() {
   sel_button_time = millis();
-  if (sel_button_time - last_sel_button_time > DEBOUNCE)
-  {
+  if (sel_button_time - last_sel_button_time > DEBOUNCE) {
     selectionMade = true;
     Serial.println(selectionMade);
     last_sel_button_time = sel_button_time;
   }
 }
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
 
   pinMode(INCREMENT_BUTTON, INPUT_PULLUP);
@@ -433,81 +383,92 @@ void setup()
   Serial.print(RXCnt);
 
   u8g2.begin();
-  u8g2.setFont(u8g2_font_7x13B_tf); // choose a suitable font
+  u8g2.setFont(u8g2_font_7x13B_tf);  // choose a suitable font
   Serial.print("Setup Complete");
 }
 
-void loop()
-{
+void loop() {
   static char buffer[50];
   static byte previousSelection = 1;
   static byte currentState = MAIN_MENU;
 
-  switch (currentState)
-  {
-  case (MAIN_MENU):
+  switch (currentState) {
+    case (MAIN_MENU):
 
-    // State info
-    sprintf(buffer, "Main Menu -> State: %d, Sel: %d, PreSel: %d", currentState, currentSelection, previousSelection);
-    Serial.println(buffer);
+      // State info
+      sprintf(buffer, "Main Menu -> State: %d, Sel: %d, PreSel: %d", currentState, currentSelection, previousSelection);
+      Serial.println(buffer);
 
-    // Limit Selection
-    if (currentSelection >= MAIN_MENU_LENGTH)
-    {
-      currentSelection = 0;
-    }
+      // Limit Selection
+      if (currentSelection >= MAIN_MENU_LENGTH) {
+        currentSelection = 0;
+      }
 
-    // Display Menu
-    if (previousSelection != currentSelection)
-    {
-      displayMenu(MAIN_MENU_OPTIONS, MAIN_MENU_LENGTH);
-      previousSelection = currentSelection;
-    }
+      // Display Menu
+      if (previousSelection != currentSelection) {
+        displayMenu(MAIN_MENU_OPTIONS, MAIN_MENU_LENGTH);
+        previousSelection = currentSelection;
+      }
 
-    // Handle selections
-    if (selectionMade && currentSelection == LIST_PEERS_SEL)
-    {
-      currentState = LIST_PEERS;
-      previousSelection = currentSelection + 1; // Make sure new menu is displayed
-      selectionMade = false;
-    }
-    else
-    {
-      selectionMade = false;
-    }
+      // Handle selections
+      if (selectionMade && currentSelection == LIST_PEERS_SEL) {
+        currentState = LIST_PEERS;
+        previousSelection = currentSelection + 1;  // Make sure new menu is displayed
+        selectionMade = false;
+      } else {
+        selectionMade = false;
+      }
 
-    break;
+      break;
 
-  case (LIST_PEERS):
+    case (LIST_PEERS):
 
-    // State info
-    // sprintf(buffer, "List Peers Menu -> State: %d, Sel: %d, PreSel: %d", currentState, currentSelection, previousSelection);
-    // Serial.println(buffer);
+      //State info
+      sprintf(buffer, "List Peers Menu -> State: %d, Sel: %d, PreSel: %d", currentState, currentSelection, previousSelection);
+      Serial.println(buffer);
 
-    // Limit Selection
-    if (currentSelection >= RXCnt + BACK_BUTTON_SPACER)
-    {
-      currentSelection = 0;
-    }
+      // Limit Selection
+      if (currentSelection >= RXCnt + BACK_BUTTON_SPACER) {
+        currentSelection = 0;
+      }
 
-    if (previousSelection != currentSelection)
-    {
-      displayPeers();
-      previousSelection = currentSelection;
-    }
+      if (previousSelection != currentSelection) {
+        displayPeers();
+        previousSelection = currentSelection;
+      }
 
-    // Handle selection
-    if (selectionMade && currentSelection == RXCnt /*Back Button Pressed*/)
-    {
-      currentState = MAIN_MENU;
-      previousSelection = currentSelection + 1; // Make sure new menu is displayed
-      selectionMade = false;
-    }
-    else
-    {
-      selectionMade = false;
-    }
+      // Handle selection
+      if (selectionMade && currentSelection == RXCnt /*Back Button Pressed*/) {
+        currentState = MAIN_MENU;
+        previousSelection = currentSelection + 1;  // Make sure new menu is displayed
+        selectionMade = false;
+      } else if (selectionMade) {
+        currentState = SELECT_EFFECT;
+        previousSelection = currentSelection + 1;  // Make sure new menu is displayed
+        selectionMade = false;
+      } else {
+        selectionMade = false;
+      }
 
-    break;
+      break;
+
+    case (SELECT_EFFECT):
+
+      //State info
+      sprintf(buffer, "Select Effect Menu -> State: %d, Sel: %d, PreSel: %d", currentState, currentSelection, previousSelection);
+      Serial.println(buffer);
+
+      byte RX_Selected = currentSelection;
+
+      // Limit Selection
+      if (currentSelection >= SELECT_EFFECT_LENGTH) {
+        currentSelection = 0;
+      }
+
+      if (previousSelection != currentSelection) {
+        displayMenu(SEL_EFFECT_OPTIONS, SELECT_EFFECT_LENGTH);
+        previousSelection = currentSelection;
+      }
+      break;
   }
 }
